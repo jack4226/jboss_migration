bbb8f03509cc0e191447794ec1c2cb06c3c58a08
jwang <jwang@twang-pc.localdomain>
2017-06-14 18:57:47 -0400

    Signed-off-by: jwang <jwang@twang-pc.localdomain>

--- MsgUI2/src/main/java/jpa/msgui/converter/NullableStringConverter.java
+++ MsgUI2/src/main/java/jpa/msgui/converter/NullableStringConverter.java
@@ -13,12 +13,11 @@ import org.apache.commons.lang3.StringUtils;
 public class NullableStringConverter implements Converter {
 
 	@Override
-	public Object getAsObject(FacesContext context, UIComponent comp,
-			String value) throws ConverterException {
+	public Object getAsObject(FacesContext context, UIComponent comp, String value) throws ConverterException {
 		if (StringUtils.isBlank(value)) {
-            if (comp instanceof EditableValueHolder) {
-                ((EditableValueHolder) comp).setSubmittedValue(null);
-            }
+			if (comp instanceof EditableValueHolder) {
+				((EditableValueHolder) comp).setSubmittedValue(null);
+			}
 			return null;
 		}
 		else {
@@ -27,8 +26,7 @@ public class NullableStringConverter implements Converter {
 	}
 
 	@Override
-	public String getAsString(FacesContext context, UIComponent component,
-			Object object) throws ConverterException {
+	public String getAsString(FacesContext context, UIComponent component, Object object) throws ConverterException {
 		if (object == null) {
 			return null;
 		}
